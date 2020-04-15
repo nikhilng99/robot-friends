9baeddbd0a70e5cc4807ee2c934f70831ae88887
Nikhil N G <62535502+nikhilng99@users.noreply.github.com>
2020-04-15 12:39:29 +0530

    Robot Friends

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                    return (
+                        <Card
+                           
+                            name={robots[i].name}
+                            email={robots[i].email}
+                            id={robots[i].id}
+                            key={i}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+}
+export default CardList
